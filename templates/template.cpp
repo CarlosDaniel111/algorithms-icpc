1ccf857a6d9dcc07b39101092caf6da63e8377c6
AngelOlanC <99060518+AngelOlanC@users.noreply.github.com>
2023-02-27 20:39:00 -0600

    Cambios
    Agregué el macro all(x) y eb

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
 
+#define all(x) x.begin(), x.end()
+#define eb emplace_back 
 #define FOR(x, n) for (long long x = 0; x < (long long)n; x++)
 #define FOR1(x, n) for (long long x = 1; x <= (long long)n; x++)
 #define FORR(x, n) for (long long x = n - 1; x >= 0; x--)
 #define FORR1(x, n) for (long long x = n; x >= 1; x--)
-#define eb emplace_back
-#define all(x) x.begin(), x.end()
-const long long INF = 1 << 28;
-const long long MOD = 1e9 + 7;
-const long long MAXN = 1e5 + 5;
+const long long INF = 1 << 28, MOD = 1e9 + 7, long long MAXN = 1e5 + 5;
 
 using namespace std;
 
@@ -19,8 +17,7 @@ typedef vector<int> vi;
 typedef vector<pair<int, int>> vii;
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(nullptr);
+    ios_base::sync_with_stdio(0); cin.tie(nullptr);
 
     return 0;
 }
