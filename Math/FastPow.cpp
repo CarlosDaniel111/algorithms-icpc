2969893b8ae83a365cae646d41688ea7e713e8d4
AngelOlanC <99060518+AngelOlanC@users.noreply.github.com>
2023-02-27 23:28:09 -0600

    Update FastPow.cpp
    Removed the recursive binpow and added matrix exponentiation code

--- Math/FastPow.cpp
+++ Math/FastPow.cpp
@@ -8,13 +8,32 @@ ll binpow(ll a, ll b, ll m){ //(a^b) mod m
   }
   return res;
 }
-//recursive way
-ll binpow(ll a, ll b, ll m){
-  if (b == 0) return 1;
 
-  ll ans = binpow(a, b / 2, m) % m;
-  ans = (ans * ans) % m;
-  if (b & 1) ans = (ans * a) % m;
+//Let A be an n*n order matrix and k the exponent, we can calculate A^k in O(log k * n^3)
+typedef vector<vi> vvi;
+//A * B = C, O(n^3)
+vvi matrixMultiplication(vvi& A, vvi& B){
+    int n = A.size(), m = A[0].size(), k = B[0].size();
+    vvi C(n, vi(k, 0));
 
-  return ans;
+    FOR(i, n)
+        FOR(j, k)
+            FOR(l, m)
+                C[i][j] += (A[i][l] * B[l][j]) % MOD;
+    return C;
 }
+//A^k, O(log k * n^3)
+vvi matrixExponentiation(vvi &A, ll k){
+    int n = A.size();
+    //ret -> identity matrix
+    vvi ret(n, vi(n)), B = A;
+    FOR(i, n) ret[i][i] = 1;
+
+    while(k){
+        if (k & 1) ret = matrixMultiplication(ret, B);
+        k >>= 1;
+        B = matrixMultiplication(B, B);
+    }
+    return ret;
+}
+//Another faster approach could be use structs with fixed matrices overloading the * operator
