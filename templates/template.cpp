e8d58bfb281552eb530188774d0a1a9d8069343d
CarlosDaniel111 <beltranmed.carlos03@gmail.com>
2023-02-27 19:51:03 -0700

    FIX

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 
 #define all(x) x.begin(), x.end()
-#define eb emplace_back 
+#define eb emplace_back
 #define FOR(x, n) for (long long x = 0; x < (long long)n; x++)
 #define FOR1(x, n) for (long long x = 1; x <= (long long)n; x++)
 #define FORR(x, n) for (long long x = n - 1; x >= 0; x--)
 #define FORR1(x, n) for (long long x = n; x >= 1; x--)
-const long long INF = 1 << 28, MOD = 1e9 + 7, long long MAXN = 1e5 + 5;
+const long long INF = 1 << 28, MOD = 1e9 + 7, MAXN = 1e5 + 5;
 
 using namespace std;
 
@@ -17,7 +17,8 @@ typedef vector<int> vi;
 typedef vector<pair<int, int>> vii;
 
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(nullptr);
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
 
     return 0;
 }
