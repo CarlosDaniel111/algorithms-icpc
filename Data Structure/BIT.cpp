8b54551a0678920c0c4fdb37ae72c5af0d889bb9
CarlosDaniel111 <beltranmed.carlos03@gmail.com>
2023-03-03 15:55:23 -0700

    Add BIT

---
+++ Data Structure/BIT.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+#define eb emplace_back
+#define FOR(x, n) for (long long x = 0; x < (long long)n; x++)
+#define FOR1(x, n) for (long long x = 1; x <= (long long)n; x++)
+#define FORR(x, n) for (long long x = n - 1; x >= 0; x--)
+#define FORR1(x, n) for (long long x = n; x >= 1; x--)
+const long long INF = 1 << 28, MOD = 1e9 + 7, MAXN = 1e5 + 5;
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> llll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pair<int, int>> vii;
+
+int bit[MAXN]; // Utilizar a partir del 1
+int n;
+
+int query(int index) {
+    int sum = 0;
+    while (index > 0) {
+        sum += bit[index];
+        index -= index & (-index);
+    }
+    return sum;
+}
+
+void update(int index, int val) {
+    while (index <= n) {
+        bit[index] += val;
+        index += index & (-index);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    return 0;
+}
