4e44c6c9b7e78f09981b93390be7ea588bc869db
AngelOlanC <99060518+AngelOlanC@users.noreply.github.com>
2023-03-02 22:18:46 -0600

    Se añadieron algunas operaciones útiles con manipulación de bits

---
+++ Math/Operaciones con bits.cpp
@@ -0,0 +1,21 @@
+//Tomando en cuenta un número x e índices i, j (0-indexados); podemos hacer muchas cosas:
+
+/*Tomando en cuenta el i-esimo bit, retorna:
+0                       Si está apagado
+Cualquier otro valor    Si está encendido*/
+x & (1 << i);
+
+//Apagar (si está encendido) el i-esimo bit
+x &= ~(1 << i);
+
+//Prender (si está apagado) el i-esimo bit
+x |= (1 << i);
+
+//Invierte el i-esimo bit
+x ^ (1 << i);
+
+//Apaga los primeros i bits
+x &= (-1 << i);
+
+//Apaga los bits en el rango [i, j]
+x &= (((~0) << (j + 1)) | ((1 << i) - 1));
