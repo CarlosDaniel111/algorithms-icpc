3305cbb41dd8b93a5f52309e302ead4e4e8dfe58
AngelOlanC <99060518+AngelOlanC@users.noreply.github.com>
2023-03-02 22:28:54 -0600

    Update Operaciones con bits.cpp

--- Math/Operaciones con bits.cpp
+++ Math/Operaciones con bits.cpp
@@ -1,14 +1,14 @@
-//Tomando en cuenta un n˙mero x e Ìndices i, j (0-indexados); podemos hacer muchas cosas:
+//Tomando en cuenta un n√∫mero x e √≠ndices i, j (0-indexados); podemos hacer muchas cosas:
 
 /*Tomando en cuenta el i-esimo bit, retorna:
-0                       Si est· apagado
-Cualquier otro valor    Si est· encendido*/
+0                       Si est√° apagado
+Cualquier otro valor    Si est√° encendido*/
 x & (1 << i);
 
-//Apagar (si est· encendido) el i-esimo bit
+//Apagar (si est√° encendido) el i-esimo bit
 x &= ~(1 << i);
 
-//Prender (si est· apagado) el i-esimo bit
+//Prender (si est√° apagado) el i-esimo bit
 x |= (1 << i);
 
 //Invierte el i-esimo bit
@@ -19,3 +19,8 @@ x &= (-1 << i);
 
 //Apaga los bits en el rango [i, j]
 x &= (((~0) << (j + 1)) | ((1 << i) - 1));
+
+/*Retorna:
+1 si x es impar
+0 si x es par*/
+x & 1
