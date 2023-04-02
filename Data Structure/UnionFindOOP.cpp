826229d819c48fe0276a0ddc7f4d4b49597443d4
Yosef-E <62635493+Yosef-E@users.noreply.github.com>
2023-04-02 14:17:16 -0600

    Add files via upload

---
+++ Data Structure/UnionFindOOP.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pd = pair<double, double>;
+
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vpd = vector<pd>;
+
+// pairs
+#define mp make_pair
+#define f first
+#define s second
+
+// vectors
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define ins insert
+#define ft front()
+#define bk back()
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define tcT template <class T tcT > int lwb(V<T> &a, const T &b) { return int(lb(all(a), b) - bg(a)); }
+
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+
+#define ENDL '\n'
+
+const int MOD = 1e9 + 7;
+const int MAXN = 1e5 + 5;
+const int INF = 1 << 28;
+const ll LLINF = 1e18;
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+class UnionFind {
+    private: vi p, rank;
+    public:
+        UnionFind(int N) {
+            rank.assign(N, 0);
+            p.assign(N, 0);
+            for(int i = 0; i < N; i++) {
+                p[i] = i;
+            }
+        }
+        int get(int i) {
+            return (p[i] == i) ? i : (p[i] = get(p[i]));
+        }
+        bool isSameSet(int i, int j) {
+            return get(i) == get(j);
+        }
+        void unionSet(int i, int j) {
+            if(!isSameSet(i, j)) {
+                int x = get(i), y = get(j);
+                if(rank[x] > rank[y]) {
+                    p[y] = x;
+                }else {
+                    p[x] = y;
+                    if(rank[x] == rank[y]) {
+                        rank[y]++;
+                    }
+                }
+            }
+        }
+};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+    int nodes; cin>>nodes;
+    UnionFind UF(nodes);
+    return 0;
+}
