bb1927376562b4681741c8bbf67c725f11b74ed6
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-04-03 22:55:18 -0600

    Update KthAncestor.cpp

--- Graphs/KthAncestor.cpp
+++ Graphs/KthAncestor.cpp
@@ -1,22 +1,4 @@
-#include <bits/stdc++.h>
-
-#define all(x) x.begin(), x.end()
-#define eb emplace_back
-#define FOR(x, n) for (long long x = 0; x < (long long)n; x++)
-#define FOR1(x, n) for (long long x = 1; x <= (long long)n; x++)
-#define FORR(x, n) for (long long x = n - 1; x >= 0; x--)
-#define FORR1(x, n) for (long long x = n; x >= 1; x--)
-const long long INF = 1 << 28, MOD = 1e9 + 7, MAXN = 1e5 + 5;
-
-using namespace std;
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> ii;
-typedef pair<ll, ll> llll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<pair<int, int>> vii;
+const int MAXN = 1e5 + 5;
 
 //Arreglo donde up[i][k], es el 2^k ancestro del nodo i
 int up[MAXN][31];
@@ -35,14 +17,14 @@ int main(){
   ios_base :: sync_with_stdio(0); cin.tie(nullptr);
 
   int n; cin >> n;
-  FOR(i, n){
+  F0R(i, n){
       int padreDelNodoI; cin >> padreDelNodoI;
       up[i][0] = padreDelNodoI;
   }
 
   //Se basa en: mi ancestro 4 es a su vez ancestro 2 de mi ancestro 2
   //Precalculo en O(n log n)
-  for(int j = 1; j < 31; j++)
-      for(int i = 0; i < n; i++)
+  FOR (j, 1, 31)
+      F0R (i, n)
           up[i][j] = up[up[i][j - 1]][j - 1];
 }
