2299bfda53950c03ff76225e8ca32a4891ce04c5
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-04-21 21:04:18 -0600

    Update Catalan.py

--- Math/Catalan.py
+++ Math/Catalan.py
@@ -6,7 +6,7 @@ catalan = [0 for i in range(510)]
 def precalculate():
     catalan[0] = 1
     for i in range(509):
-        catalan[i + 1] = (((2*(2*i+1))/(i+2)) * catalan[i])
+        catalan[i + 1] = ((2*(2*i+1) * catalan[i])/(i+2))
 
 
 precalculate()
