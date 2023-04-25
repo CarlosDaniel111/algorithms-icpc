ef30845c7a7e3f99f8203cefb10ab2fe1ce71522
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-04-25 00:25:25 -0600

    Update DP_con_digitos.cpp

--- DP/DP_con_digitos.cpp
+++ DP/DP_con_digitos.cpp
@@ -4,33 +4,33 @@ string s; // cadena
 int num;
 stack<int> st;
 
-bool memo[MAXN][MAXN];
-bool dp(int i, int r){
-    if (memo[i][r]) return false;
+bool dp[MAXN][MAXN];
+bool solve(int i, int r){
+    if (dp[i][r]) return false;
     if (i == s.length()) return r == 0;
  
     if (s[i] == '?'){ 
         for (int k = (i == 0); k <= 9; k++){
-            if (dp(i+1, (r*10 + k) % num)) {
+            if (solve(i+1, (r*10 + k) % num)) {
                 st.push(k);
                 return true;
             }
         } 
     }
     else{
-        if (dp(i+1, (r*10 + (s[i]-'0')) % num)) {
+        if (solve(i+1, (r*10 + (s[i]-'0')) % num)) {
             st.push(s[i]-'0');
             return true;
         }
     }   
-    memo[i][r] = true;
+    dp[i][r] = true;
     return false; 
 }
  
 int main() { 
     cin >> s >> num;
  
-    if(dp(0, 0)){
+    if(solve(0, 0)){
         while(!st.empty()){
             cout << st.top();
             st.pop();
