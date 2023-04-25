8bc6ab6f6f564573a2ec0f967db1c464189339f7
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-04-25 16:24:33 -0600

    Update Formulas_Rapidas.cpp

--- Math/Formulas_Rapidas.cpp
+++ Math/Formulas_Rapidas.cpp
@@ -6,18 +6,13 @@
     gcd(m,n)
     lcm(m,n)
 */
-
-int gcd(int a, int b) {
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
-}
-
+int gcd(int a, int b) { return (b ? gcd(b, a % b) : a); }
 ll lcm(int a, int b) { return ((a * b) / gcd(a, b)); }
 
 ll fastpow(ll a, ll b, ll m) { //(a^b) mod m
     ll res = 1;
     a %= m;
+    b %= m;
     while (b) {
         if (b & 1)
             res = (res * a) % m;
@@ -27,7 +22,22 @@ ll fastpow(ll a, ll b, ll m) { //(a^b) mod m
     return res;
 }
 
-//Siendo b un entero positivo y p un primo
-int modInverse(int b, int p) {
-    return fastpow(b, p - 2, p) % p;
+// Codigo para calcular el inverso de a modulo m (si es que existe)
+
+// Si m es primo
+int modInverse(int b, int m) { return fastpow(b, m - 2, m) % m; }
+
+// Si m NO es primo
+using iii = tuple<int, int, int>;
+
+iii extendedGCD(int a, int b){
+    if (b == 0) return Triple({a, 1, 0});
+    auto [d, x, y] = extendedGCD(b, a % b);
+    return {d, y, x - a/b * y};
+}
+
+int modInverse(int a, int m){
+    auto [d, x, y] = extendedGCD(a, m);
+    if (d > 1) return 0; // Si no existe
+    return (x + m) % m;
 }
