3a7862173abe4b8bf0423dd539a2aa20dea0d08a
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-04-24 20:46:18 -0600

    Update LCA.cpp

--- Graphs/LCA.cpp
+++ Graphs/LCA.cpp
@@ -1,24 +1,3 @@
-#include <bits/stdc++.h>
-
-#define all(x) x.begin(), x.end()
-#define eb emplace_back
-#define ENDL '\n'
-#define FOR(x, n) for (long long x = 0; x < (long long)n; x++)
-#define FOR1(x, n) for (long long x = 1; x <= (long long)n; x++)
-#define FORR(x, n) for (long long x = n - 1; x >= 0; x--)
-#define FORR1(x, n) for (long long x = n; x >= 1; x--)
-const long long INF = 1 << 28, MOD = 1e9 + 7, MAXN = 1e5 + 5;
-
-using namespace std;
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> ii;
-typedef pair<ll, ll> llll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<pair<int, int>> vii;
-
 const LOG_MAXN = 25;
 vi tree[MAXN];
 int salto[MAXN][LOG_MAXN];
@@ -35,10 +14,8 @@ void dfs(int u, int padre = -1, int d = 0) {
 
 void build(int n) {
     // Inicializar salto en -1
-    FOR(i, n)
-    FOR(j, LOG_MAXN)
-    salto[i][j] = -1;
-
+    memset(salto, -1, sizeof salto);
+    
     dfs(0);
 
     // Construccion del binary-lifting
@@ -52,7 +29,7 @@ int LCA(int p, int q) {
     if (depth[p] < depth[q])
         swap(p, q);
 
-    int dist = depth[p] - depth[q]; // Distancia necesario para estar en la misma profundidad
+    int dist = depth[p] - depth[q]; // Distancia necesaria para estar en la misma profundidad
     FORR(i, LOG_MAXN)
     if ((dist >> i) & 1)
         p = salto[p][i];
@@ -68,11 +45,4 @@ int LCA(int p, int q) {
     }
 
     return salto[p][0];
-}
-
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(nullptr);
-
-    return 0;
 }
