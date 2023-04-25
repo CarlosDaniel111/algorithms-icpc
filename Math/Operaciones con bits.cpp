a933e4d8d4c80658e6d5d8f0e9ea989a333bde1e
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-04-24 20:55:09 -0600

    Update Operaciones con bits.cpp

--- Math/Operaciones con bits.cpp
+++ Math/Operaciones con bits.cpp
@@ -1,5 +1,5 @@
 // NOTA - Si i > 30, usar 1LL
-// Tomando en cuenta un número x e índices i, j (0-indexados); podemos hacer muchas cosas:
+// Siendo S un numero y {i, j} indices 0-indexados:
 
 #define isOn(S, j) (S & (1 << j))
 #define setBit(S, j) (S |= (1 << j))
@@ -8,41 +8,12 @@
 #define lowBit(S) (S & (-S))
 #define setAll(S, n) (S = (1 << n) - 1)
 
-#define modulo(S, N) ((S) & (N - 1)) // returns S % N, where N is a power of 2
+#define modulo(S, N) ((S) & (N - 1)) // retorna S % N, siendo N una potencia de 2
+#define isOdd(S) (s & 1)
 #define isPowerOfTwo(S) (!(S & (S - 1)))
 #define nearestPowerOfTwo(S) (1 << lround(log2(S)))
 #define turnOffLastBit(S) ((S) & (S - 1))
 #define turnOnLastZero(S) ((S) | (S + 1))
+#define turnOffInRange(S, i, j) s &= (((~0) << (j + 1)) | ((1 << i) - 1));
 #define turnOffLastConsecutiveBits(S) ((S) & (S + 1))
 #define turnOnLastConsecutiveZeroes(S) ((S) | (S - 1))
-
-/*Tomando en cuenta el i-esimo bit, retorna:
-0                       Si está apagado
-Cualquier otro valor    Si está encendido*/
-x & (1 << i);
-
-// Apagar (si está encendido) el i-esimo bit
-x &= ~(1 << i);
-
-// Prender (si está apagado) el i-esimo bit
-x |= (1 << i);
-
-// Invierte el i-esimo bit
-x ^ (1 << i);
-
-// Apaga los primeros i bits
-x &= (-1 << i);
-
-// Apaga los bits en el rango [i, j]
-x &= (((~0) << (j + 1)) | ((1 << i) - 1));
-
-/*Retorna:
-1 si x es impar
-0 si x es par*/
-x & 1;
-
-// Recorre a x k bits a la derecha, lo podemos ver como (x >> k) = piso(x / (2^k))
-x >> k;
-
-// Recorre a x k bits a la izquierda, lo podemos ver como (x << k) = x * (2^k)
-x << k;
