8418ba7fa3f4ebd7ce98472932f81b9ca66e5823
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-04-25 16:31:18 -0600

    Update LCA.cpp

--- Graphs/LCA.cpp
+++ Graphs/LCA.cpp
@@ -1,28 +1,27 @@
 const LOG_MAXN = 25;
 vi tree[MAXN];
-int salto[MAXN][LOG_MAXN];
+int jump[MAXN][LOG_MAXN];
 int depth[MAXN];
 
 // DFS para calcular la profundidad y guardar el padre directo en salto[u][0]
 void dfs(int u, int padre = -1, int d = 0) {
     depth[u] = d;
-    salto[u][0] = padre;
+    jump[u][0] = padre;
     for (auto &hijo : tree[u])
         if (hijo != padre)
             dfs(hijo, u, d + 1);
 }
 
 void build(int n) {
-    // Inicializar salto en -1
-    memset(salto, -1, sizeof salto);
+    memset(jump, -1, sizeof jump);
     
     dfs(0);
 
     // Construccion del binary-lifting
     for (int i = 1; i < LOG_MAXN; i++)
         for (int u = 0; u < n; u++)
-            if (salto[u][i - 1] != -1)
-                salto[u][i] = salto[salto[u][i - 1]][i - 1];
+            if (jump[u][i - 1] != -1)
+                jump[u][i] = jump[jump[u][i - 1]][i - 1];
 }
 
 int LCA(int p, int q) {
@@ -32,17 +31,17 @@ int LCA(int p, int q) {
     int dist = depth[p] - depth[q]; // Distancia necesaria para estar en la misma profundidad
     FORR(i, LOG_MAXN)
     if ((dist >> i) & 1)
-        p = salto[p][i];
+        p = jump[p][i];
 
     if (p == q) // Verificar si el ancestro es la misma profundidad
         return p;
 
     // Busqueda por saltos binarios
     FORR(i, LOG_MAXN)
-    if (salto[p][i] != salto[q][i]) {
-        p = salto[p][i];
-        q = salto[q][i];
+    if (jump[p][i] != jump[q][i]) {
+        p = jump[p][i];
+        q = jump[q][i];
     }
 
-    return salto[p][0];
+    return jump[p][0];
 }
