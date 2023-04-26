409ed69670df9a609371e79668e3d64014662186
CarlosDaniel111 <beltranmed.carlos03@gmail.com>
2023-04-25 17:20:16 -0700

    Notebook 1.0

--- Notebook/code/Formulas_Rapidas.cpp
+++ Notebook/code/Formulas_Rapidas.cpp
@@ -22,6 +22,13 @@ ll fastpow(ll a, ll b, ll m) { //(a^b) mod m
     return res;
 }
 
+// Coeficientes binomiales (Combinatoria)
+ll C(int n, int k) { // O(log p)
+    if (n < k)
+        return 0;
+    return (((fact[n] * modInverse(fact[k])) % p) * modInverse(fact[n - k])) % p;
+}
+
 // Codigo para calcular (a^-1)%m (si es que existe)
 
 // Si m es primo
@@ -30,14 +37,16 @@ int modInverse(int b, int m) { return fastpow(b, m - 2, m) % m; }
 // Si m NO es primo
 using iii = tuple<int, int, int>;
 
-iii extendedGCD(int a, int b){
-    if (b == 0) return Triple({a, 1, 0});
+iii extendedGCD(int a, int b) {
+    if (b == 0)
+        return Triple({a, 1, 0});
     auto [d, x, y] = extendedGCD(b, a % b);
-    return {d, y, x - a/b * y};
+    return {d, y, x - a / b * y};
 }
 
-int modInverse(int a, int m){
+int modInverse(int a, int m) {
     auto [d, x, y] = extendedGCD(a, m);
-    if (d > 1) return 0; // Si no existe
+    if (d > 1)
+        return 0; // Si no existe
     return (x + m) % m;
 }
