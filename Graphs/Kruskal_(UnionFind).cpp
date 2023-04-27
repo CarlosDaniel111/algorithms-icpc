e290ed6b682e828ac00448ca6479b2a0cb6dc925
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-04-26 23:40:00 -0600

    Update Kruskal_(UnionFind).cpp

--- Graphs/Kruskal_(UnionFind).cpp
+++ Graphs/Kruskal_(UnionFind).cpp
@@ -4,13 +4,17 @@ class UnionFind {
     private: int numSets, parent[MAXN], rank[MAXN], setSize[MAXN];
     public:
         UnionFind(int &N) {
-            for(int i = 0; i < N; i++) 
+            for(int i = 0; i < N; i++) { 
                 parent[i] = i;
+                setSize[i] = 1;
+            }
             numSets = N;
         }
         int get(int i) { return (parent[i] == i) ? i : (parent[i] = get(parent[i])); }
         bool isSame(int i, int j) { return get(i) == get(j); }
         int sizeOfSet(int i) { return setSize[get(i)]; }
+        int numOfSets() { return numSets; }
+        
         void unite(int i, int j) {
             if(!isSame(i, j)) {
                 int x = get(i), y = get(j);
