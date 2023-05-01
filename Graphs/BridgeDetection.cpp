3788db5dbca469e4e485f8848deaabcba43ecfbf
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-04-30 21:39:26 -0600

    Update BridgeDetection.cpp

--- Graphs/BridgeDetection.cpp
+++ Graphs/BridgeDetection.cpp
@@ -1,18 +1,18 @@
-int n;                   // number of nodes
-vector<vector<int>> adj; // adjacency list of graph
+const int MAXN = 1e6 + 5;
 
-vector<bool> articulation;
-vector<int> tin, low;
-int timer, dfsRoot, rootChildren;
+int n;                   // number of nodes
+vector<int> adj[MAXN]; // adjacency list of graph
+bool articulation[MAXN];
+int tin[MAXN], low[MAXN], timer, dfsRoot, rootChildren;
 
 void dfs(int u, int p = -1) {
     tin[u] = low[u] = timer++;
     for (int to : adj[u]) {
         if (to == p)
             continue;
-        if (tin[to] != -1) {
+        if (tin[to] != -1)
             low[u] = min(low[u], tin[to]);
-        } else {
+        else {
             if (u == dfsRoot)
                 ++rootChildren; // Caso especial si es raiz
 
@@ -30,9 +30,9 @@ void dfs(int u, int p = -1) {
 
 void find_bridges_articulations() {
     timer = 0;
-    tin.assign(n, -1);
-    low.assign(n, -1);
-    articulation.assign(n, 0);
+    fill(tin, tin + n, - 1);
+    fill(low, low + n, - 1);
+    
     for (int i = 0; i < n; ++i) {
         if (tin[i] == -1) {
             dfsRoot = i;
