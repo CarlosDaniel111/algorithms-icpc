b5090b8d02f29dddadbbf6b75e6e1da022d8a9ce
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-04-30 21:36:59 -0600

    Update Kruskal_(UnionFind).cpp

--- Graphs/Kruskal_(UnionFind).cpp
+++ Graphs/Kruskal_(UnionFind).cpp
@@ -1,19 +1,15 @@
 const int MAXN = 1e5 + 5;
 
 class UnionFind {
-    private: int numSets, parent[MAXN], rank[MAXN], setSize[MAXN];
     public:
+        int numSets, parent[MAXN], rank[MAXN], setSize[MAXN];
         UnionFind(int &N) {
-            for(int i = 0; i < N; i++) { 
-                parent[i] = i;
-                setSize[i] = 1;
-            }
+            iota(parent, parent + N, 0);
+            fill(setSize, setSize + N, 1);
             numSets = N;
         }
         int get(int i) { return (parent[i] == i) ? i : (parent[i] = get(parent[i])); }
         bool isSame(int i, int j) { return get(i) == get(j); }
-        int sizeOfSet(int i) { return setSize[get(i)]; }
-        int numOfSets() { return numSets; }
         
         void unite(int i, int j) {
             if(!isSame(i, j)) {
@@ -22,11 +18,10 @@ class UnionFind {
                 parent[x] = y;
                 if (rank[x] == rank[y]) ++rank[y];
                 setSize[y] += setSize[x];
-                --numSets;
+                numSets--;
             }
         }
 };
-
 
 using Edge = tuple<int, int, int>;
 
@@ -48,7 +43,7 @@ int main() {
     sort(edges, edges + E);
 
     int totalWeight = 0;
-    for (int i = 0; i < E && UF.numOfSets() > 1; i++) {
+    for (int i = 0; i < E && UF.numSets > 1; i++) {
         auto [w, u, v] = edges[i]; // desempaquetamiento de arista
         if (!UF.isSame(u, v)) {       // Si no estan en el mismo conjunto, la tomamos
             totalWeight += w;
