3367ee90196a3273b4f9c94c90d3b6728f0bd382
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-05-09 21:36:33 -0600

    Create Tecnica_deque.cpp

---
+++ DP/Tecnica_deque.cpp
@@ -0,0 +1,17 @@
+// Retorna un vector b en donde b[i] es igual a
+// max(a[i], a[i + 1], a[i + 2], ..., a[i + p - 1]);
+
+vi max_1D(vi a, int p) {
+  vi b(sz(a) - p + 1);
+  deque<int> dq;
+  F0R (i, sz(a)) {
+        while (sz(dq) && a[dq.bk] <= a[i])
+            dq.pop_back();
+        dq.pb(i);
+        if (dq.ft <= i - p) 
+            dq.pop_front();
+        
+        if (i + 1 >= p)
+            b[i + 1 - p] = a[dq.ft];
+  }
+}
