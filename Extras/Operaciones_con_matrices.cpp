421df5cf61b10352e8174b808d7350f7dabbc915
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-05-09 21:50:43 -0600

    Update Operaciones_con_matrices.cpp

--- Extras/Operaciones_con_matrices.cpp
+++ Extras/Operaciones_con_matrices.cpp
@@ -1,6 +1,6 @@
-// Let A be an n*n order matrix and k the exponent, we can calculate A^k in O(log k * n^3)
+// Sea A una matriz de orden n*n y k un numero, podemos calcular A^k en O(log k * n^3)
 typedef vector<vi> vvi;
-// A * B = C, O(n^3)
+// O(n^3)
 vvi matrixMultiplication(vvi &A, vvi &B) {
     int n = A.size(), m = A[0].size(), k = B[0].size();
     vvi C(n, vi(k, 0));
@@ -11,11 +11,11 @@ vvi matrixMultiplication(vvi &A, vvi &B) {
     C[i][j] += (A[i][l] * B[l][j]) % MOD;
     return C;
 }
-// A^k, O(log k * n^3)
+// O(log k * n^3)
 vvi matrixExponentiation(vvi &A, ll k) {
     int n = A.size();
-    // ret -> identity matrix
-    vvi ret(n, vi(n)), B = A;
+    
+    vvi ret(n, vi(n)), B = A; // Matriz identidad
     FOR(i, n)
     ret[i][i] = 1;
 
@@ -30,7 +30,7 @@ vvi matrixExponentiation(vvi &A, ll k) {
 // Si el problema es el tiempo, implementar struct Matrix
 
 /*
-Approach para calcular el n-esimo fibonacci en O(log n):
+Estrategia para calcular el n-esimo fibonacci en O(log n):
   Utilizar la matriz de fibonacci:
 A = {1, 1} ^ n
     {1, 0}
