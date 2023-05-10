b21902eacb1ba56a1cacf80192056c4cd5f75d1c
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-05-09 22:01:38 -0600

    Update Formulas_Rapidas.cpp

--- Math/Formulas_Rapidas.cpp
+++ Math/Formulas_Rapidas.cpp
@@ -9,7 +9,8 @@
 int gcd(int a, int b) { return (b ? gcd(b, a % b) : a); }
 ll lcm(int a, int b) { return ((a * b) / gcd(a, b)); }
 
-ll fastpow(ll a, ll b, ll m) { //(a^b) mod m
+// (a^b) mod m, O(log n)
+ll fastpow(ll a, ll b, ll m) {
     ll res = 1;
     a %= m;
     while (b) {
@@ -21,14 +22,14 @@ ll fastpow(ll a, ll b, ll m) { //(a^b) mod m
     return res;
 }
 
-// Coeficientes binomiales (Combinatoria)
+// Coeficientes binomiales (Combinatoria) - O(n)
 ll C(int n, int k) { // O(log p)
     if (n < k)
         return 0;
     return (((fact[n] * modInverse(fact[k])) % p) * modInverse(fact[n - k])) % p;
 }
 
-// Codigo para calcular (a^-1)%m (si es que existe)
+// Codigo para calcular (a^-1)%m (si es que existe), O(log n)
 
 // Si m es primo
 int modInverse(int b, int m) { return fastpow(b, m - 2, m) % m; }
