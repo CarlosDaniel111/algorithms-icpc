9e037dea5fefb628f0098b83044a503c41e5d664
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-06-29 14:08:25 -0700

    Update templatecpp.cpp

--- templates/templatecpp.cpp
+++ templates/templatecpp.cpp
@@ -5,8 +5,8 @@ using ll = long long;
 using pi = pair<int, int>;
 using vi = vector<int>;
 
-#define f first
-#define s second
+#define fi first
+#define se second
 #define pb push_back
 #define SZ(x) int((x).size())
 #define ALL(x) begin(x), end(x)
