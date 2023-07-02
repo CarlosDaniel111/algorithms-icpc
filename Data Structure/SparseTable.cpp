bff79081488357eab251d9a922b240736e99312f
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-07-02 15:01:45 -0700

    Create SparseTable.cpp

---
+++ Data Structure/SparseTable.cpp
@@ -0,0 +1,29 @@
+// La Sparse Table es una estructura de datos que nos permite realizar operaciones
+// conmutativas sobre un arreglo estatico en O(1) con una construccion de O(n logn)
+// ST[k][i] almacena el {op} en el rango [i, i + 2^k - 1]
+
+template <typename T> 
+struct SparseTable {
+    vector<vector<T>> ST;
+
+    T op(T &a, T &b) {
+        return min(a, b);
+    }
+
+    void build(vector<T> &A) {
+        ST.assign(30, vector<T>(SZ(A), 0));
+
+        for (int i = 0; i < SZ(A); i++)
+            ST[0][i] = A[i];
+        
+        for (int k = 1; k < 30; k++)
+            for (int i = 0; i + (1 << k) <= SZ(A); i++) 
+                ST[k][i] = op(ST[k - 1][i], ST[k - 1][i + (1 << (k - 1))]);
+    }
+
+    // [l, r)
+    T query(int l, int r) {
+        int p = 31 - __builtin_clz(r-l);
+        return op(ST[p][l], ST[p][r - (1 << p)]);
+    }
+};
