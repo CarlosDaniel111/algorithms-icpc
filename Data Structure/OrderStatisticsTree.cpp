2337fea4883ecda43c6bf21b7d9d9ca17713aa2e
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-07-02 15:31:52 -0700

    Update and rename Order_Statistics_Tree.cpp to OrderStatisticsTree.cpp

--- Data Structure/OrderStatisticsTree.cpp
+++ Data Structure/OrderStatisticsTree.cpp
@@ -5,17 +5,15 @@ using namespace std;
 using namespace __gnu_pbds;
 
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ost;
-/*(Posiciones indexadas en 0).
-Funciona igual que un set (todas las operaciones en O(log n)), con 2 operaciones extra:
+/*
+Funciona igual que un set, con 2 operaciones extra en O(log n):
 obj.find_by_order(k) - Retorna un iterador apuntando al elemento k-esimo mas grande
 obj.order_of_key(x) - Retorna un entero que indica la cantidad de elementos menores a x
 
-Modificar unicamente primer y tercer parametro, que corresponden a el tipo de dato
-del ost y a la funcion de comparacion de valores (less<T>, greater<T>, less_equal<T>
-o incluso una implementada por nosotros)
+Modificar primer y tercer parametro, correspondientes al tipo de dato del ost 
+y a la funcion comparadora (less<T>, greater<T>, less_equal<T> o incluso una propia)
 
-Si queremos elementos repetidos, usar less_equal<T> (sin embargo, ya no servira la
-funcion de eliminacion).
+Si queremos elementos repetidos y no necesitamos eliminacion de valores, usar less_equal<T>.
 
 Si queremos elementos repetidos y necesitamos la eliminacion, utilizar una
 tecnica con pares, donde el second es un numero unico para cada valor.
