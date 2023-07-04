a432b692f1b7cfe14dab7ef8d214d3dbd29a5dec
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-07-04 10:59:55 -0700

    Create FindCentroid.cpp

---
+++ Graphs/FindCentroid.cpp
@@ -0,0 +1,13 @@
+int dfs(int u, int p) {
+    for (auto v : tree[u])
+        if (v != p) 
+            subtreeSZ[u] += dfs(v, u);
+    return subtreeSZ[u] += 1;
+}
+
+int centroid(int u, int p) {
+    for (auto v : tree[u])
+        if (v != p && subtreeSZ[v] * 2 > n) 
+            return centroid(v, u);
+    return u;
+}
