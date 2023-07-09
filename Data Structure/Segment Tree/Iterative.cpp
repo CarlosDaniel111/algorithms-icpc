9c56e01a52f28a859e121e27873c965976936d5f
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-07-08 21:25:00 -0700

    Update Iterative.cpp

--- Data Structure/Segment Tree/Iterative.cpp
+++ Data Structure/Segment Tree/Iterative.cpp
@@ -1,4 +1,4 @@
-// Implementado para minimos, es posible cambiar a cualquier operacion conmutativa
+// Implementado para minimo, es posible cambiar a cualquier operacion conmutativa
 template <class T> class SegmentTree {
   private:
     const T DEFAULT = 1e18;  // Causa overflow si T es int
@@ -17,17 +17,17 @@ template <class T> class SegmentTree {
         ind += len;
         ST[ind] = val;
         for (; ind > 1; ind /= 2) {
-            ST[ind / 2] = min(ST[ind], ST[ind ^ 1]);
+            ST[ind / 2] = min(ST[ind], ST[ind ^ 1]); // Operacion
         }
     }
 
     T query(int start, int end) {
         end++;
-        T sum = DEFAULT;
+        T ans = DEFAULT;
         for (start += len, end += len; start < end; start /= 2, end /= 2) {
-            if (start % 2 == 1) { sum = min(sum, ST[start++]); }
-            if (end % 2 == 1) { sum = min(sum, ST[--end]); }
+            if (start % 2 == 1) { ans = min(ans, ST[start++]); } // Operacion
+            if (end % 2 == 1) { ans = min(ans, ST[--end]); } // Operacion
         }
-        return sum;
+        return ans;
     }
 };
