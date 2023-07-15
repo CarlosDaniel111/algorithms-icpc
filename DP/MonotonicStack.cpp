78ec9c7f55fba1a63a7c1247bd81c251de4506cb
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-07-14 23:27:27 -0700

    Update and rename TecnicaConPila.cpp to MonotonicStack.cpp

--- DP/MonotonicStack.cpp
+++ DP/MonotonicStack.cpp
@@ -1,4 +1,4 @@
-#define F0R(i, a) for (int i = 0; i < n; i++)
+// Usando la tecnica de la pila monotona para calcular para cada indice, el elemento menor a la izquierda
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -6,7 +6,7 @@ int main() {
 
     int n = 12, heights[n] = {1, 8, 4, 9, 9, 10, 3, 2, 4, 8, 1, 13}, leftSmaller[n];
     stack<int> st;
-    F0R(i, n) {
+    FOR (i, 0, n) {
         while (!st.empty() && heights[st.top()] > heights[i])
             st.pop();
         if (st.empty())
@@ -15,5 +15,4 @@ int main() {
             leftSmaller[i] = st.top();
         st.push(i);
     }
-    // Ahora leftSmaller[i] tiene el indice del elemento menor mas cercano a la izquierda de heights[i]
 }
