d0cb45fb5a59748ff1c7786b7fe4475820195a7b
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-07-21 19:53:18 -0700

    Update Kruskal.cpp

--- Graphs/Kruskal.cpp
+++ Graphs/Kruskal.cpp
@@ -1,20 +1,4 @@
-struct DSU {  // Indice base 0
-    vi e;
-    void init(int N) { e = vi(N, -1); }
-    int get(int x) { return e[x] < 0 ? x : e[x] = get(e[x]); }
-    bool sameSet(int a, int b) { return get(a) == get(b); }
-    int size(int x) { return -e[get(x)]; }
-    bool unite(int x, int y) {  // union by size
-        x = get(x), y = get(y);
-        if (x == y)
-            return 0;
-        if (e[x] > e[y])
-            swap(x, y);
-        e[x] += e[y];
-        e[y] = x;
-        return 1;
-    }
-};
+#include<../Data Structure/DSU.h>
 
 using Edge = tuple<int, int, int>;
 
