aedf2f88d6cc58e9504479a48ea32e31cdb8a9a6
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-07-21 19:47:10 -0700

    Create UF.cpp

---
+++ Data Structure/UF.cpp
@@ -0,0 +1,17 @@
+struct DSU {
+    vi e;
+    void init(int N) { e = vi(N, -1); }
+    int get(int x) { return e[x] < 0 ? x : e[x] = get(e[x]); }
+    bool sameSet(int a, int b) { return get(a) == get(b); }
+    int size(int x) { return -e[get(x)]; }
+    bool unite(int x, int y) {
+        x = get(x), y = get(y);
+        if (x == y)
+            return 0;
+        if (e[x] > e[y])
+            swap(x, y);
+        e[x] += e[y];
+        e[y] = x;
+        return 1;
+    }
+};
