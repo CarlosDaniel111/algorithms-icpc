5a3090b31df179ff680042ad242e298786567802
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-08-10 15:50:37 -0700

    Update Bits.cpp

--- Math/Bits.cpp
+++ Math/Bits.cpp
@@ -1,5 +1,11 @@
-// NOTA - Si i > 30, usar 1LL
-// Siendo S un numero y {i, j} indices 0-indexados:
+/**
+ * Algunas operaciones utiles con desplazamiento de bits,
+ * si trabajamos con numeros de 64 bits, usar 1LL o 1ULL,
+ * siendo la primer parte operaciones nativas y la segunda
+ * del compilador GNU (GCC) que necesitan ll al final del
+ * nombre del metodo, si no se trabaja con enteros
+ * Tiempo por operacion: O(1)
+*/
 
 #define isOn(S, j) (S & (1 << j))
 #define setBit(S, j) (S |= (1 << j))
@@ -7,7 +13,6 @@
 #define toggleBit(S, j) (S ^= (1 << j))
 #define lowBit(S) (S & (-S))
 #define setAll(S, n) (S = (1 << n) - 1)
-
 #define modulo(S, N) ((S) & (N - 1))  // retorna S % N, siendo N una potencia de 2
 #define isOdd(S) (s & 1)
 #define isPowerOfTwo(S) (!(S & (S - 1)))
@@ -17,33 +22,20 @@
 #define turnOffInRange(S, i, j) s &= (((~0) << (j + 1)) | ((1 << i) - 1));
 #define turnOffLastConsecutiveBits(S) ((S) & (S + 1))
 #define turnOnLastConsecutiveZeroes(S) ((S) | (S - 1))
-/*
-Si n <= 20, podemos iterar sobre todos los subconjuntos:
+
+#define countBitsOn(n) __builtin_popcount(x);
+#define firstBitOn(n) __builtin_ffs(x);
+#define countLeadingZeroes(n) __builtin_clz(n)
+#define log2Floor(n) 31 - __builtin_clz(n)
+#define countTrailingZeroes(n) __builtin_ctz(n)
+
+/**
+ * Si n <= 20 y manejamos subconjuntos, podemos revisar cada uno de ellos
+ * representandolos como una mascara de bits, en donde el i-esimo elemento
+ * es tomado si el i-esimo bit esta encendido
+ * Tiempo: O(2^n)
+ */
 int LIMIT = 1 << (n + 1);
 for (int i = 0; i < LIMIT; i++) {
-  ....
+    
 }
-*/
-
-// Funciones integradas por el compilador GNU (GCC)
-// IMPORTANTE ---> Si x cabe en un int quitar el ll de cada metodo :D
-
-// Numero de bits encendidos de x
-__builtin_popcountll(x);
-
-// Indice del primer (de derecha a izquierda) bit encendido de x
-// Por ejemplo __builtin_ffs(0b0001'0010'1100) = 3
-__builtin_ffsll(x);
-
-// Cuenta de ceros a la izquierda del primer bit encendido de x
-// Utilizado para calcular piso(log2(x)) -> 63 - __builtin_clzll(x)
-// Si x es int, utilizar 31 en lugar de 63
-// Por ejemplo __builtin_clz(0b0001'0010'1100) = 23 (YA QUE X SE TOMA COMO ENTERO)
-__builtin_clzll(x);
-
-// Cuenta de ceros a la derecha del primer uno (de derecha a izquierda
-// Por ejemplo __builtin_ctzll(0b0001'0010'1100) = 2
-__builtin_ctzll(x);
-
-// Regresa piso(log2(n)), usar 63 si se usa long long
-31 - __builtin_clz(n)
