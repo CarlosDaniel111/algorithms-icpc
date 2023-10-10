1ddd9f2b397d5b29c08f36afeb4a3051bfb3159b
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-10-09 18:14:35 -0700

    Update SparseTable.cpp

--- DataStructure/SparseTable.cpp
+++ DataStructure/SparseTable.cpp
@@ -10,19 +10,19 @@ struct SparseTable {
   vector<vector<T>> ST;
 
   void build(vector<T> &A) {
-    const int MAX_LOG = __builtin_clz(A.size()) + 2;
+    const int MAX_LOG = 25;
     ST.assign(MAX_LOG, vector<T>(SZ(A), 0));
 
     for (int i = 0; i < SZ(A); i++)
       ST[0][i] = A[i];
 
-    for (int k = 1; k < MAX_LOG; k++)
+    for (int k = 1; k <= MAX_LOG; k++)
       for (int i = 0; i + (1 << k) <= SZ(A); i++)
         ST[k][i] = min(ST[k - 1][i], ST[k - 1][i + (1 << (k - 1))]);
   }
 
   T query(int l, int r) {  // [l, r]
-    int p = 31 - __builtin_clz(r - l);
+    int p = 31 - __builtin_clz(r - l + 1);
     return min(ST[p][l], ST[p][r - (1 << p) + 1]);
   }
 };
