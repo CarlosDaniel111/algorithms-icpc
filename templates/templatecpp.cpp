d194a2ecb4f33717f668382fe2001357cd9e983e
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2023-12-19 22:57:13 -0700

    Update templatecpp.cpp

--- templates/templatecpp.cpp
+++ templates/templatecpp.cpp
@@ -1,27 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-// AC2++
+// BRO
 using ll = long long;
 using pi = pair<int, int>;
 using vi = vector<int>;
 
-#define fi first
-#define se second
 #define pb push_back
 #define SZ(x) ((int)(x).size())
 #define ALL(x) begin(x), end(x)
-#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
-#define ROF(i, a, b) for (int i = (a)-1; i >= (b); --i)
+#define FOR(i, a, b) for (int i = (int)a; i < (int)b; ++i)
+#define ROF(i, a, b) for (int i = (int)a-1; i >= (int)b; --i)
 #define ENDL '\n'
+#define __   ios_base::sync_with_stdio(0); cin.tie(nullptr);
 
-constexpr int MOD = 1e9 + 7;
-constexpr int MAXN = 1e5 + 5;
-constexpr int INF = 1e9;
-constexpr ll LLINF = 1e18;
+int main() { __
 
-int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(nullptr);
 
   return 0;
 }
