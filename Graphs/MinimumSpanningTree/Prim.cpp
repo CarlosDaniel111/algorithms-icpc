c39952537cc3b60539b8053102ffedabb96da9c4
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2024-01-18 14:39:37 -0700

    Update Prim.cpp

--- Graphs/MinimumSpanningTree/Prim.cpp
+++ Graphs/MinimumSpanningTree/Prim.cpp
@@ -1,30 +1,24 @@
 /**
  * Descripcion: tiene como principal funcion calcular la suma del
  * peso de las aristas del arbol minimo de expansion (MST) de un grafo,
- * la estrategia es ir construyendo gradualmente el MST, se selecciona un
+ * la estrategia es ir construyendo gradualmente el MST, se inicia con un
  * nodo arbitrario y se agregan sus aristas con nodos que no hayan
  * sido agregados con anterioridad y se va tomando la de menor peso hasta
  * completar el MST.
  * Tiempo: O(E log E)
  */
+int prim(vector<vector<pi>>& g) {
+  vector<bool> taken(SZ(g), 0);
+  priority_queue<pi> pq;
 
-int V, E;
-vector<vector<pi>> graph;
-vector<bool> taken;
-priority_queue<pi> pq;
-
-void process(int u) {
-  taken[u] = 1;
-  for (auto &[v, w] : graph[u])
-    if (!taken[v])
-      pq.push({-w, v});
-}
-
-int prim() {
-  taken.assign(SZ(graph), 0);
+  auto process = [&](int u) -> void {
+    taken[u] = 1;
+    for (auto &[v, w] : g[u]) if (!taken[v]) pq.push({-w, v});
+  };
+  
   process(0);
   int totalWeight = 0, takenEdges = 0;
-  while (!pq.empty() && takenEdges != V - 1) {
+  while (!pq.empty() && takenEdges != SZ(g) - 1) {
     auto [w, u] = pq.top();
     pq.pop();
 
