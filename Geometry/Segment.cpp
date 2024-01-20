b3410341019813951ca6b28af6fb0a6834e5f61f
CarlosDaniel111 <beltranmed.carlos03@gmail.com>
2024-01-20 15:26:24 -0700

    Update Segment

--- Geometry/Segment.cpp
+++ Geometry/Segment.cpp
@@ -1,65 +1,51 @@
+// Retorna si el Punto P se encuentra dentro del circulo entre A y B
+bool inDisk(Point a, Point b, Point p) { return (a - p).dot(b - p) <= 0; }
+
 // Retorna si el punto P se encuentra en el segmento de puntos S a E
-bool onSegment(Point s, Point e, Point p) {
-  return cross(p, s, e) == 0 && dot(s - p, e - p) <= 0;
+bool onSegment(Point a, Point b, Point p) {
+  return a.cross(b, p) == 0 && inDisk(a, b, p);
 }
 
-// Retorna la distancia mas corta entre el punto P y el segmento S a E
-double dist2(Point x) { return x * x + y * y; }
-double dist(Point x) { sqrt((double)dist2(x)); }
-double pointToSegDist(Point& s, Point& e, Point& p) {
-  if (s == e) return dist(p - s);
-  auto d = dist2(e - s), t = min(d, max(.0, dot(p - s, e - s)));
-  return (dist((p - s) * d - (e - s) * t) / d);
-}
-
-// Retorna la distancia mas corta entre el punto P y el segmento AB
-// y guarda el punto mas cercano en C
-double pointToSegDist(Point p, Point a, Point b, Point& c) {
-  vec ap = toVector(a, p), ab = toVector(a, b);
-  double u = dot(ap, ab) / sq(ab);
-  if (u < 0.0) {
-    c = Point{a.x, a.y};
-    return dist(p, a);
-  }
-  if (u > 1.0) {
-    c = Point{b.x, b.y};
-    return dist(p, b);
-  }
-  return distToLine(p, a, b, c);
-}
-
-bool properInter(pt a, pt b, pt c, pt d, pt &out) {
+// SEGMENTO - SEGMENTO INTERSECCION
+bool properInter(Point a, Point b, Point c, Point d, Point& p) {
   double oa = orient(c, d, a),
-  ob = orient(c, d, b),
-  oc = orient(a, b, c),
-  od = orient(a, b, d);
-  // Proper intersection exists iff opposite signs
+         ob = orient(c, d, b),
+         oc = orient(a, b, c),
+         od = orient(a, b, d);
   if (oa * ob < 0 && oc * od < 0) {
-    out = (a * ob - b * oa) / (ob - oa);
+    p = (a * ob - b * oa) / (ob - oa);
     return true;
   }
   return false;
 }
-
-// Retorna la distancia entre los segmentos AB y CD
-double segToSegDist(pt a, pt b, pt c, pt d) {
-  pt dummy;
-  if (properInter(a,b,c,d,dummy)) return 0;
-  return min({pointToSegDist(a,b,c), pointToSegDist(a,b,d), pointToSegDist(c,d,a), pointToSegDist(c,d,b)});
-}
-
 // Si existe un punto de interseccion unico entre los segmentos de linea que van de A a B y de C a D, se devuelve.
 // Si no existe ningun punto de interseccion, se devuelve un vector vacio.
 // Si existen infinitos, se devuelve un vector con 2 elementos, que contiene los puntos finales del segmento de linea comun.
 vector<Point> segInter(Point a, Point b, Point c, Point d) {
-  auto oa = cross(c, d, a), ob = cross(c, d, b),
-       oc = cross(a, b, c), od = cross(a, b, d);
-  if (sgn(oa) * sgn(ob) < 0 && sgn(oc) * sgn(od) < 0)
-    return {(a * ob - b * oa) / (ob - oa)};
+  Point p;
+  if (properInter(a, b, c, d, p)) return {p};
   set<Point> s;
   if (onSegment(c, d, a)) s.insert(a);
   if (onSegment(c, d, b)) s.insert(b);
   if (onSegment(a, b, c)) s.insert(c);
   if (onSegment(a, b, d)) s.insert(d);
   return {ALL(s)};
+}
+
+// SEGMENTO - PUNTO DISTANCIA
+double segPoint(Point a, Point b, Point p) {
+  if (a != b) {
+    Line l(a, b);
+    if (l.cmpProj(a, p) && l.cmpProj(p, b))
+      return l.dist(p);
+  }
+  return min((p - a).norm(), (p - b).norm());
+}
+
+// SEGMENTO - SEGMENTO DISTANCIA
+double segSeg(Point a, Point b, Point c, Point d) {
+  Point dummy;
+  if (properInter(a, b, c, d, dummy)) return 0;
+  return min({segPoint(a, b, c), segPoint(a, b, d),
+              segPoint(c, d, a), segPoint(c, d, b)});
 }
