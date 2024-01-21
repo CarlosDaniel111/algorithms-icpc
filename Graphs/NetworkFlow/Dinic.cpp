108d6fb742959bedb8505c1d77f45d9fbffe0a6f
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2024-01-20 20:38:18 -0700

    Update Dinic.cpp

--- Graphs/NetworkFlow/Dinic.cpp
+++ Graphs/NetworkFlow/Dinic.cpp
@@ -3,48 +3,46 @@
  * Tiempo: O(V^2 E)
  */
 
+template<typename T>
 struct Dinic {
+  #define INF numeric_limits<T>::max()
   struct Edge {
     int to, rev;
-    ll c, oc;
-    ll flow() { return max(oc - c, 0LL); }  // if you need flows
+    T c, oc;
+    T flow() { return max(oc - c, T(0)); }  // if you need flows
   };
   vi lvl, ptr, q;
   vector<vector<Edge>> adj;
   Dinic(int n) : lvl(n), ptr(n), q(n), adj(n) {}
-  void addEdge(int a, int b, ll c, ll rcap = 0) {
+  void addEdge(int a, int b, T c, T rcap = 0) {
     adj[a].push_back({b, SZ(adj[b]), c, c});
     adj[b].push_back({a, SZ(adj[a]) - 1, rcap, rcap});
   }
-  ll dfs(int v, int t, ll f) {
+  T dfs(int v, int t, T f) {
     if (v == t || !f) return f;
     for (int& i = ptr[v]; i < SZ(adj[v]); i++) {
       Edge& e = adj[v][i];
       if (lvl[e.to] == lvl[v] + 1)
-        if (ll p = dfs(e.to, t, min(f, e.c))) {
+        if (T p = dfs(e.to, t, min(f, e.c))) {
           e.c -= p, adj[e.to][e.rev].c += p;
           return p;
         }
     }
     return 0;
   }
-  ll calc(int s, int t) {
-    ll flow = 0;
+  T calc(int s, int t) {
+    T flow = 0;
     q[0] = s;
-    FOR(L, 0, 31)
-    do {  // 'int L=30' maybe faster for random data
+    FOR(L, 0, 31) do {  // 'int L=30' maybe faster for random data
       lvl = ptr = vi(SZ(q));
       int qi = 0, qe = lvl[s] = 1;
       while (qi < qe && !lvl[t]) {
         int v = q[qi++];
-        for (Edge e : adj[v])
-          if (!lvl[e.to] && e.c >> (30 - L))
-            q[qe++] = e.to, lvl[e.to] = lvl[v] + 1;
+        for (Edge e : adj[v]) if (!lvl[e.to] && e.c >> (30 - L)) q[qe++] = e.to, lvl[e.to] = lvl[v] + 1;
       }
-      while (ll p = dfs(s, t, LLONG_MAX)) flow += p;
+      while (T p = dfs(s, t, INF)) flow += p;
     }
-    while (lvl[t])
-      ;
+    while (lvl[t]);
     return flow;
   }
   bool leftOfMinCut(int a) { return lvl[a] != 0; }
