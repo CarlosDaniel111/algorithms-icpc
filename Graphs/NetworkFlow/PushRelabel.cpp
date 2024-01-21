6f6e8a0b2f1229cba6d3735b682401f698cb45ca
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2024-01-20 20:32:02 -0700

    Update PushRelabel.cpp

--- Graphs/NetworkFlow/PushRelabel.cpp
+++ Graphs/NetworkFlow/PushRelabel.cpp
@@ -4,25 +4,24 @@
  * Tiempo: $O(V^2\sqrt E)$
  */
 
+template<typename T>
 struct PushRelabel {
   struct Edge {
     int dest, back;
-    ll f, c;
+    T f, c;
   };
   vector<vector<Edge>> g;
-  vector<ll> ec;
+  vector<T> ec;
   vector<Edge*> cur;
   vector<vi> hs;
   vi H;
   PushRelabel(int n) : g(n), ec(n), cur(n), hs(2 * n), H(n) {}
-
-  void addEdge(int s, int t, ll cap, ll rcap = 0) {
+  void addEdge(int s, int t, T cap, T rcap = 0) {
     if (s == t) return;
     g[s].push_back({t, SZ(g[t]), 0, cap});
     g[t].push_back({s, SZ(g[s]) - 1, 0, rcap});
   }
-
-  void addFlow(Edge& e, ll f) {
+  void addFlow(Edge& e, T f) {
     Edge& back = g[e.dest][e.back];
     if (!ec[e.dest] && f) hs[H[e.dest]].push_back(e.dest);
     e.f += f;
@@ -32,7 +31,7 @@ struct PushRelabel {
     back.c += f;
     ec[back.dest] -= f;
   }
-  ll calc(int s, int t) {
+  T calc(int s, int t) {
     int v = SZ(g);
     H[s] = v;
     ec[t] = 1;
@@ -40,25 +39,18 @@ struct PushRelabel {
     co[0] = v - 1;
     FOR (i, 0, v) cur[i] = g[i].data();
     for (Edge& e : g[s]) addFlow(e, e.c);
-
     for (int hi = 0;;) {
-      while (hs[hi].empty())
-        if (!hi--) return -ec[s];
+      while (hs[hi].empty()) if (!hi--) return -ec[s];
       int u = hs[hi].back();
       hs[hi].pop_back();
-      while (ec[u] > 0)  // discharge u
-        if (cur[u] == g[u].data() + SZ(g[u])) {
-          H[u] = 1e9;
-          for (Edge& e : g[u])
-            if (e.c && H[u] > H[e.dest] + 1)
-              H[u] = H[e.dest] + 1, cur[u] = &e;
-          if (++co[H[u]], !--co[hi] && hi < v)
-            FOR (i, 0, v) if (hi < H[i] && H[i] < v)-- co[H[i]], H[i] = v + 1;
-          hi = H[u];
-        } else if (cur[u]->c && H[u] == H[cur[u]->dest] + 1)
-          addFlow(*cur[u], min(ec[u], cur[u]->c));
-        else
-          ++cur[u];
+      while (ec[u] > 0) if (cur[u] == g[u].data() + SZ(g[u])) { // discharge u
+        H[u] = 1e9;
+        for (Edge& e : g[u]) if (e.c && H[u] > H[e.dest] + 1) H[u] = H[e.dest] + 1, cur[u] = &e;
+        if (++co[H[u]], !--co[hi] && hi < v) FOR (i, 0, v) if (hi < H[i] && H[i] < v)-- co[H[i]], H[i] = v + 1;
+        hi = H[u];
+      } 
+      else if (cur[u]->c && H[u] == H[cur[u]->dest] + 1) addFlow(*cur[u], min(ec[u], cur[u]->c));
+      else ++cur[u];
     }
   }
   bool leftOfMinCut(int a) { return H[a] >= SZ(g); }
