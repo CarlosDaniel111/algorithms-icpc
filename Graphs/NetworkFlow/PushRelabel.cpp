54e8cbb8820560423447a0de5a408fc5a7420d72
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2024-01-20 20:20:10 -0700

    Update PushRelabel.cpp

--- Graphs/NetworkFlow/PushRelabel.cpp
+++ Graphs/NetworkFlow/PushRelabel.cpp
@@ -18,8 +18,8 @@ struct PushRelabel {
 
   void addEdge(int s, int t, ll cap, ll rcap = 0) {
     if (s == t) return;
-    g[s].push_back({t, sz(g[t]), 0, cap});
-    g[t].push_back({s, sz(g[s]) - 1, 0, rcap});
+    g[s].push_back({t, SZ(g[t]), 0, cap});
+    g[t].push_back({s, SZ(g[s]) - 1, 0, rcap});
   }
 
   void addFlow(Edge& e, ll f) {
@@ -33,12 +33,12 @@ struct PushRelabel {
     ec[back.dest] -= f;
   }
   ll calc(int s, int t) {
-    int v = sz(g);
+    int v = SZ(g);
     H[s] = v;
     ec[t] = 1;
     vi co(2 * v);
     co[0] = v - 1;
-    rep(i, 0, v) cur[i] = g[i].data();
+    FOR (i, 0, v) cur[i] = g[i].data();
     for (Edge& e : g[s]) addFlow(e, e.c);
 
     for (int hi = 0;;) {
@@ -47,13 +47,13 @@ struct PushRelabel {
       int u = hs[hi].back();
       hs[hi].pop_back();
       while (ec[u] > 0)  // discharge u
-        if (cur[u] == g[u].data() + sz(g[u])) {
+        if (cur[u] == g[u].data() + SZ(g[u])) {
           H[u] = 1e9;
           for (Edge& e : g[u])
             if (e.c && H[u] > H[e.dest] + 1)
               H[u] = H[e.dest] + 1, cur[u] = &e;
           if (++co[H[u]], !--co[hi] && hi < v)
-            rep(i, 0, v) if (hi < H[i] && H[i] < v)-- co[H[i]], H[i] = v + 1;
+            FOR (i, 0, v) if (hi < H[i] && H[i] < v)-- co[H[i]], H[i] = v + 1;
           hi = H[u];
         } else if (cur[u]->c && H[u] == H[cur[u]->dest] + 1)
           addFlow(*cur[u], min(ec[u], cur[u]->c));
@@ -61,5 +61,5 @@ struct PushRelabel {
           ++cur[u];
     }
   }
-  bool leftOfMinCut(int a) { return H[a] >= sz(g); }
+  bool leftOfMinCut(int a) { return H[a] >= SZ(g); }
 };
