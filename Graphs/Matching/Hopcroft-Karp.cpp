fd2d66318dfeecc3e728c8de9041bd4ab7e4f297
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2024-01-23 00:33:27 -0700

    Update Hopcroft-Karp.cpp

--- Graphs/Matching/Hopcroft-Karp.cpp
+++ Graphs/Matching/Hopcroft-Karp.cpp
@@ -6,47 +6,45 @@
  * emparejamiento para el vertice i del lado derecho o -1 si no lo tiene
  * Tiempo: O(sqrt(V)E)
  */
-
-bool dfs(int a, int L, vector<vi>& g, vi& btoa, vi& A, vi& B) {
-	if (A[a] != L) return 0;
-	A[a] = -1;
-	for (int b : g[a]) if (B[b] == L + 1) {
-		B[b] = 0;
-		if (btoa[b] == -1 || dfs(btoa[b], L + 1, g, btoa, A, B)) return btoa[b] = a, 1;
-	}
-	return 0;
-}
-
 pair<int, vi> hopcroftKarp(vector<vi>& g, int m) {
-	int res = 0;
-	vi btoa(m, -1), A(SZ(g)), B(m), cur, next;
+  int res = 0;
+  vi btoa(m, -1), A(SZ(g)), B(m), cur, next;
+  auto dfs = [&](auto self, int a, int L) -> bool {
+    if (A[a] != L) return 0;
+    A[a] = -1;
+    for (int b : g[a]) if (B[b] == L + 1) {
+      B[b] = 0;
+      if (btoa[b] == -1 || self(self, btoa[b], L + 1)) return btoa[b] = a, 1;
+    }
+    return 0;
+  };
   while (1) {
-		fill(ALL(A), 0);
-		fill(ALL(B), 0);
-		/// Encuentra los nodos restantes para BFS (i.e. con layer 0)
-		cur.clear();
-		for (int a : btoa) if(a != -1) A[a] = -1;
-		FOR (a, 0, SZ(g)) if(A[a] == 0) cur.pb(a);
-		/// Encunetra todas las layers usando BFS
-		for (int lay = 1;; lay++) {
-			bool islast = 0;
-			next.clear();
-			for (int a : cur) for (int b : g[a]) {
-				if (btoa[b] == -1) {
-					B[b] = lay;
-					islast = 1;
-				}
-				else if (btoa[b] != a && !B[b]) {
-					B[b] = lay;
-					next.pb(btoa[b]);
-				}
-			}
-			if (islast) break;
-			if (next.empty()) return {res, btoa};
-			for (int a : next) A[a] = lay;
-			cur.swap(next);
-		}
-		/// Usa DFS para escanear caminos aumentantes
-		FOR (a, 0, SZ(g)) res += dfs(a, 0, g, btoa, A, B);
-	}
+    fill(ALL(A), 0);
+    fill(ALL(B), 0);
+    /// Encuentra los nodos restantes para BFS (i.e. con layer 0)
+    cur.clear();
+    for (int a : btoa) if(a != -1) A[a] = -1;
+    FOR (a, 0, SZ(g)) if(A[a] == 0) cur.pb(a);
+    /// Encunetra todas las layers usando BFS
+    for (int lay = 1;; lay++) {
+      bool islast = 0;
+      next.clear();
+      for (int a : cur) for (int b : g[a]) {
+        if (btoa[b] == -1) {
+          B[b] = lay;
+          islast = 1;
+        }
+        else if (btoa[b] != a && !B[b]) {
+          B[b] = lay;
+          next.pb(btoa[b]);
+        }
+      }
+      if (islast) break;
+      if (next.empty()) return {res, btoa};
+      for (int a : next) A[a] = lay;
+      cur.swap(next);
+    }
+    /// Usa DFS para escanear caminos aumentantes
+    FOR (a, 0, SZ(g)) res += dfs(dfs, a, 0);
+  }
 }
