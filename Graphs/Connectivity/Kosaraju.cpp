cdcb6e8ca709e04181ecb16019d24b4454517af1
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2024-01-22 19:44:28 -0700

    Update Kosaraju.cpp

--- Graphs/Connectivity/Kosaraju.cpp
+++ Graphs/Connectivity/Kosaraju.cpp
@@ -3,29 +3,25 @@
  * este realiza dos pasadas DFS, la primera para almacenar el orden de finalizacion
  * decreciente (orden topologico) y la segunda se realiza en un grafo transpuesto a
  * partir del orden topologico para hallar los SCC.
+ * Retorna el vector de los SCC, donde SCC[i] es el vector de los nodos del i-esimo SCC
  * Tiempo: O(V + E)
  */
-
-vector<vi> g, gT;
-vi dfs_num, S;
-int numSCC;
-
-void go(int u, int pass) {
-  dfs_num[u] = 1;
-  vi &neighbor = (pass == 1) ? g[u] : gT[u];
-  for (auto v : neighbor) if (dfs_num[v] == -1) go(v, pass);
-  S.pb(u);
-}
-
-int korasaju() {
-  S.clear();
-  dfs_num.assign(SZ(g), -1);
-  FOR (u, 0, SZ(g)) if (dfs_num[u] == -1) go(u, 1);
-  dfs_num.assign(SZ(g), -1);
-  numSCC = 0;
-  FOR (i, 0, SZ(g)) if (dfs_num[S[i]] == -1) {
-    ++numSCC;
-    go(S[i], 2);
+vector<vi> korasaju(vector<vi>& g, vector<vi>& gT) {
+  int n = SZ(g), pass = 1;
+  vector<vi> scc;
+  vi last_vis(n, 0), S;
+  auto dfs = [&](auto self, int u) -> void {
+    if (pass == 2) scc.back().pb(u);
+    last_vis[u] = pass;
+    for (auto v : pass == 1 ? g[u] : gT[u]) if (last_vis[v] != pass) self(self, v);
+    if (pass == 1) S.pb(u);
+  };
+  FOR (u, 0, n) if (last_vis[u] != pass) dfs(dfs, u);
+  pass = 2;
+  reverse(ALL(S));
+  for (auto &u : S) if (last_vis[u] != pass) {
+    scc.pb({});
+    dfs(dfs, u);
   }
-  return numSCC;
+  return scc;
 }
