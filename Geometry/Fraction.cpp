44bc8b89b7c8dec3457f375e6d61a17654c2a1e8
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2024-02-24 22:04:59 -0700

    Update Fraction.cpp

--- Geometry/Fraction.cpp
+++ Geometry/Fraction.cpp
@@ -13,40 +13,16 @@ struct Frac {
       _a = -_a;
       _b = -_b;
     }
-
     int GCD = gcd(abs(_a), abs(_b));
-
     a = _a / GCD;
     b = _b / GCD;
   }
 
-  Frac operator*(Frac& other) const { return Frac(a * other.a, b * other.b); }
-  Frac operator/(Frac& other) const {
-    Frac o(other.b, other.a);
-    return (*this) * o;
-  }
-  Frac operator+(Frac& other) const {
-    int sup = a * other.b + b * other.a, inf = b * other.b;
-    return Frac(sup, inf);
-  }
-  Frac operator-(Frac& other) const {
-    int sup = a * other.b - b * other.a, inf = b * other.b;
-    return Frac(sup, inf);
-  }
-  Frac operator*(int& x) const { return Frac(a * x, b); }
-  Frac operator/(int& x) const {
-    Frac o(1, x);
-    return (*this) * o;
-  }
-  bool operator<(Frac& other) const {  // PROVISIONAL, IMPLEMENTARLA MEJOR SI HACEN FALTA LOWER BOUNDS
-    if (a != other.a)
-      return a < other.a;
-    return b < other.b;
-  }
-  bool operator==(Frac& other) const {
-    return a == other.a && b == other.b;
-  }
-  bool operator!=(Frac& other) const {
-    return !(*this == other);
-  }
+  Frac operator*(Frac f) const { return Frac(a * f.a, b * f.b); }
+  Frac operator/(Frac f) const { return (*this) * Frac(f.b, f.a);  }
+  Frac operator+(Frac f) const { return Frac(a * f.b + b * f.a, b * f.b); }
+  Frac operator-(Frac f) const { return Frac(a * f.b - b * f.a, b * f.b); }
+  bool operator<(Frac& other) const { return a * other.b < other.a * b; }
+  bool operator==(Frac& other) const { return a == other.a && b == other.b; }
+  bool operator!=(Frac& other) const { return !(*this == other); }
 };
